45ea8c4d44ba0cf19391052a96edaba36a52fd2d
Damons07 <148792685+Damons07@users.noreply.github.com>
2023-12-04 09:30:31 -0600

    Delete PROYECTOOO directory

--- PROYECTOOO/pages/GraficosDistribucion.py
+++
@@ -1,9 +0,0 @@
-# pages/GraficosDistribucion.py
-import streamlit as st
-import plotly.express as px
-
-def show(datos_filtrados):
-    st.subheader("Distribución de Ventas")
-    fig_ventas_por_pais = px.bar(datos_filtrados, x='País', y='Importe venta total',
-                                 title='Ventas por País', labels={'Importe venta total': 'Ventas'})
-    st.plotly_chart(fig_ventas_por_pais)
